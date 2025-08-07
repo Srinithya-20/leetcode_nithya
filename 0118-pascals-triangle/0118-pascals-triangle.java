708e0d843f7795e4c2ecc83e5d4e643940848f2b
Srinithya-20 <23951a67g6@iare.ac.in>
2025-08-07 10:48:42 +0530

    Time: 1 ms (86.07%), Space: 42.1 MB (59.77%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -8,11 +8,11 @@ class Solution {
             List<Integer> row = new ArrayList<>();
 
             for (int j = 0; j <= i; j++) {
-                // First and last elements are always 1
+                
                 if (j == 0 || j == i) {
                     row.add(1);
                 } else {
-                    // Sum of two values from the row above
+                    
                     int sum = triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j);
                     row.add(sum);
                 }
